4bbf2024a1c281986cac275f32c0dfeb80f9dc72
torno <tornoen@gmail.com>
2026-02-03 15:10:53 +0100

    Avien

--- models/index.js
+++ models/index.js
@@ -1,32 +1,33 @@
-const Sequelize = require('sequelize')
-const fs = require("fs")
-const path = require("path")
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
 const basename = path.basename(__filename);
-require('dotenv').config()
+require("dotenv").config();
 const connection = {
   database: process.env.DATABASE_NAME,
   username: process.env.ADMIN_USERNAME,
   password: process.env.ADMIN_PASSWORD,
+  port: process.env.DB_PORT,
   host: process.env.HOST,
   dialect: process.env.DIALECT,
   dialectmodel: process.env.DIALECTMODEL,
 };
 const sequelize = new Sequelize(connection);
-const db = {}
-db.sequelize = sequelize
+const db = {};
+db.sequelize = sequelize;
 fs.readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) &&   
-      (file.slice(-3) === '.js');
-    })
-  .forEach(file => {    
-    const model = require(path.join(__dirname, file))(sequelize,   
-      Sequelize);
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
+  })
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
     db[model.name] = model;
   });
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
-module.exports = db
+module.exports = db;
